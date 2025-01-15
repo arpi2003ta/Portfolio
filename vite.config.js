c06c62b6a480bcf828e43e41c154f8ff561a28ae
arpita <natharpita2004@gmail.com>
2025-01-16 01:26:42 +0530

    1st commit done

--- vite.config.js
+++ vite.config.js
@@ -1,32 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    chunkSizeWarningLimit: 1500, // Increased chunk size limit for flexibility
-    rollupOptions: {
-      output: {
-        manualChunks: (id) => {
-          // Dynamically split large modules into separate chunks
-          if (id.includes('node_modules')) {
-            return id
-              .toString()
-              .split('node_modules/')[1]
-              .split('/')[0]
-              .toString();
-          }
-        },
-      },
-    },
-    target: 'esnext', // Ensure compatibility with modern browsers
-  },
-  server: {
-    port: process.env.PORT || 5173, // Use environment variable for port or default to 5173
-    strictPort: true, // Enforce using the specified port
-    hmr: {
-      protocol: 'ws', // Use WebSocket for HMR
-      host: 'localhost', // Ensure HMR works correctly locally and on deployment
-    },
-  },
-});
+})
